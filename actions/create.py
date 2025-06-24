41e4f542ac82e70adba2ccc4f702430ff0525c11
Andres Lopez <ing.andlopgam5@gmail.com>
2025-06-23 18:27:41 -0700

    created adding and viewing tasks functions

--- actions/create.py
+++ actions/create.py
@@ -1,1 +1,11 @@
-# CREATE NEW TASK FUNCTION
+# ADD NEW TASKS FUNCTION
+
+def add_new_task():
+    new_task = input("Enter new task: ")
+    return new_task
+
+# CREATE VIEW TASKS FUNCTION
+
+def view_tasks(tasks):
+    for i in range(len(tasks)):
+       print(f"{i+1}. {tasks[i]}")
