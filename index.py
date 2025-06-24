4a0869a14407e21f9ec74d5654c4b68430d06726
Andres Lopez <ing.andlopgam5@gmail.com>
2025-06-23 19:31:06 -0700

    created main code for To-Do List

--- index.py
+++ index.py
@@ -1,3 +1,18 @@
+from actions.create import add_new_task, view_tasks
+from actions.menu import show_menu
+
+# FUNCTIONS
+def ask():
+    reply_num = False
+    while not reply_num:
+        reply = input("Enter your choice: ")
+        if reply.isnumeric():
+            return reply
+        else:
+            reply_num = False
+    return None
+
+
 # INITIAL VARIABLES AND LISTS
 tasks = []
 on_off = True
@@ -5,3 +20,21 @@ on_off = True
 # APP
 while on_off:
     print("To-Do List Application")
+    show_menu()
+    choice = ask()
+    if choice == "1":
+        new_task = add_new_task()
+        tasks.append(new_task)
+        print()
+    elif choice == "2":
+        print()
+        pass
+    elif choice == "3":
+        view_tasks(tasks)
+        print()
+    elif choice == "4":
+        print("Goodbye")
+        on_off = False
+    else:
+        print("Invalid choice")
+        print()
