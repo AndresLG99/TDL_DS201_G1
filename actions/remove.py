502e307bc64ff9cef1122f4307a30ab4df3bc654
Melissa Osorio <estefiosorio@hotmail.com>
2025-06-27 13:42:39 -0700

    updated range in remove

--- actions/remove.py
+++ actions/remove.py
@@ -5,15 +5,16 @@ def remove_tasks(tasks):
     view_tasks(tasks)
     sorted_tasks = sort_tasks(tasks)
     print()
+
     while True:
         try:
             choice = int(input(f"Enter the ID of the task to remove (1 - {len(sorted_tasks)}) / 0: Cancel \n"))
-            if choice in range(0, len(sorted_tasks)):
+            if choice in range(0, len(sorted_tasks)+1):
                 if choice == 0:
                     print(f"Aborting")
                     break
                 else:
-                    sorted_tasks.pop(int(choice) - 1)
+                    sorted_tasks.pop(choice - 1)
                     print(f"Task was removed successfully")
                     break
             else:
